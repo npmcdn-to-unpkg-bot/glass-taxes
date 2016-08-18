0cefd62ab3e18254c3d7f46b4401a29005bf5cd9
Patrick Varin <patrickjvarin@gmail.com>
2016-08-18 15:03:49 -0500

    Refactored TimeSeries to BarChart and created an async version that allows for lazy data loading

--- index.js
+++ index.js
@@ -1,96 +1,37 @@
+
+var test_data = [-10, -5, -2, 4, 6, 20, 7, 3, 0, -2, -4,
+                     -3, 0, 5, 3, 2, 1, 3, 8, 10];
+
 var budget_data = {
-    url: '/data/json_budget_files/Table 1.json',
+    url: '/data/json_budget_files/Table 1.3—Summary of Receipts, Outlays, and Surpluses or Deficits (-) in Current Dollars, Constant (FY 2009) Dollars, and as Percentages of GDP: 1940–2021.json',
     field_name: 'Surplus or Deficit (–) As Percentages of GDP'
 }
 
-var TimeSeries = React.createClass({
-    render: function(){
-        return (
-            <svg height={this.props.height} width={this.props.width} id={this.props.graph_id}></svg>
-        );
-    },
-    componentDidMount: function(){
-
-        //AJAX request for data
-        var httpRequest = new XMLHttpRequest();
-        httpRequest.onreadystatechange = function(resp){
-            if (resp.readyState === XMLHttpRequest.DONE){
-                // response is done
-                if (resp.status == 200){
-                    // response is good
-                    var data = JSON.parse(resp.responseText);
-                    var transformed_data = data.map(function(d){return d[budget_data['field_name']]});
-                    console.log(transformed_data);
-                    this.renderData(transformed_data);
-                }else{
-                    // response is bust
-                    console.log('Response: ',resp.status)
-                }
+var get_data = function(data_callback){
+    //AJAX request for data
+    var httpRequest = new XMLHttpRequest();
+    httpRequest.onreadystatechange = function(resp){
+        if (resp.readyState === XMLHttpRequest.DONE){
+            // response is done
+            if (resp.status == 200){
+                // response is good
+                var data = JSON.parse(resp.responseText);
+                var transformed_data = data.map(function(d){return d[budget_data['field_name']]});
+                data_callback(transformed_data);
             }
-        }.bind(this,httpRequest);
-        httpRequest.open('GET',budget_data['url']);
-        httpRequest.send(null);
-
-        // this.renderData(this.props.data)
-    },
-    renderData : function(data){
-        var padding = 30;
-
-        // figure out the x axis
-        var num_data = data.length;
-        var w = (this.props.width-2*padding)/num_data - 1;
-        var x_scale = d3.scaleLinear()
-            .domain([0, data.length-1])
-            .range ([padding,this.props.width-padding])
-
-        // figure out the y axis
-        var min_data = Math.min(0,Math.min.apply(Math,data));
-        var max_data = Math.max(0,Math.max.apply(Math,data));
-
-        var y_scale = d3.scaleLinear()
-            .domain([min_data, max_data])
-            .range([this.props.height-padding, padding]);
-        var zero_height = y_scale(0);
-        var h = function (d) {return Math.abs(y_scale(d)-y_scale(0)) || 1;};
-        var y = function (d) {return d<0 ? zero_height : zero_height - h(d);};
-
-        //locate the drawing board
-        var svg = d3.select('#'+this.props.graph_id);
-
-        //plot the axes
-        var x_axis = d3.axisBottom(x_scale)
-            .ticks(0);
-        var y_axis = d3.axisLeft(y_scale);
-
-        svg.append('g')
-            .call(x_axis)
-            .attr('transform','translate(0,'+zero_height+')');
-        svg.append('g')
-            .call(y_axis)
-            .attr('transform','translate('+padding+','+ 0 +')');
-        
-        //plot the bars
-        svg.selectAll('rect')
-            .data(data)
-            .enter()
-            .append('rect')
-            .attr('x', function(d,i){return padding+i*(w+1)})
-            .attr('y',y)
-            .attr('width', w)
-            .attr('height', h)
-            .attr('fill', function(d,i){
-                if (d<0) return 'rgb(204,0,0)'
-                else if (d>0) return 'rgb(0,153,0)'
-                else return 'black'
-            });
-    }
-});
-
+        }
+    }.bind(this,httpRequest);
+    httpRequest.open('GET',budget_data['url']);
+    httpRequest.send(null);
+}
 
 ReactDOM.render(
     <div>
         <h1>GlassTaxes</h1>
-        <TimeSeries height={300} width={500} graph_id={'total_budget_spend'}/>
+        <h2>Sample Data</h2>
+        <BarChart height={300} width={500} data={test_data} graph_id={'test_bar_chart'}/>
+        <h2>{budget_data['field_name']}</h2>
+        <AsyncBarChart height={300} width={500} graph_id={'total_budget_spend'} get_data={get_data}/>
     </div>,
     document.getElementById('content')
 )
---
+++ js/BarChart.js
@@ -0,0 +1,81 @@
+var AsyncBarChart = React.createClass({
+    render: function(){
+        return (
+            <svg height={this.props.height}
+                 width={this.props.width}
+                 id={this.props.graph_id}>
+            </svg>
+        );
+    },
+    componentDidMount: function(){
+        this.props.get_data(this.renderData)
+    },
+    renderData : function(data){
+        var padding = 30;
+
+        // figure out the x axis
+        var num_data = data.length;
+        var w = (this.props.width-2*padding)/num_data - 1;
+        var x_scale = d3.scaleLinear()
+            .domain([0, data.length-1])
+            .range ([padding,this.props.width-padding])
+
+        // figure out the y axis
+        var min_data = Math.min(0,Math.min.apply(Math,data));
+        var max_data = Math.max(0,Math.max.apply(Math,data));
+
+        var y_scale = d3.scaleLinear()
+            .domain([min_data, max_data])
+            .range([this.props.height-padding, padding]);
+        var zero_height = y_scale(0);
+        var h = function (d) {return Math.abs(y_scale(d)-y_scale(0)) || 1;};
+        var y = function (d) {return d<0 ? zero_height : zero_height - h(d);};
+
+        //locate the drawing board
+        var svg = d3.select('#'+this.props.graph_id);
+
+        //plot the axes
+        var x_axis = d3.axisBottom(x_scale)
+            .ticks(0);
+        var y_axis = d3.axisLeft(y_scale);
+
+        svg.append('g')
+            .call(x_axis)
+            .attr('transform','translate(0,'+zero_height+')');
+        svg.append('g')
+            .call(y_axis)
+            .attr('transform','translate('+padding+','+ 0 +')');
+        
+        //plot the bars
+        svg.selectAll('rect')
+            .data(data)
+            .enter()
+            .append('rect')
+            .attr('x', function(d,i){return padding+i*(w+1)})
+            .attr('y',y)
+            .attr('width', w)
+            .attr('height', h)
+            .attr('fill', function(d,i){
+                if (d<0) return 'rgb(204,0,0)'
+                else if (d>0) return 'rgb(0,153,0)'
+                else return 'black'
+            });
+    }
+});
+
+var BarChart = React.createClass({
+    render: function(){
+        var get_data = function(cb){
+            // immediately pass along the data stored in the props
+            cb(this.props.data);
+        }.bind(this);
+        return (
+            <AsyncBarChart height={this.props.height}
+                           width={this.props.width}
+                           graph_id={this.props.graph_id}
+                           get_data={get_data}/>
+        );
+    }
+});
+window.BarChart = BarChart
+window.AsyncBarChart = AsyncBarChart
