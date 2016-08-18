5729b27d6bf5e9d8eb7c8139df0788d7090bfc6a
Patrick Varin <patrickjvarin@gmail.com>
2016-08-18 01:41:11 -0500

    cleaned up TimeSeries using a linear scale for the bar heights

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var test_data = [-10, -5, -2, 4, 6, 10, 7, 3, 0, -2, -4,
+var test_data = [-10, -5, -2, 4, 6, 20, 7, 3, 0, -2, -4,
                      -3, 0, 5, 3, 2, 1, 3, 8, 10];
 
 var TimeSeries = React.createClass({
@@ -10,37 +10,27 @@ var TimeSeries = React.createClass({
     componentDidMount: function(){
         // figure out the x axis
         var num_data = this.props.data.length;
-        var width = this.props.width/num_data - 1;
-        console.log(width)
+        var w = this.props.width/num_data - 1;
 
         // figure out the y axis
-        var min_data = Math.min.apply(null,this.props.data);
-        var max_data = Math.max.apply(null,this.props.data);
-        var y_range = max_data-Math.min(0,min_data);
+        var min_data = Math.min(0,Math.min.apply(Math,this.props.data));
+        var max_data = Math.max(0,Math.max.apply(Math,this.props.data));
 
-        var zero_height;
-        if (max_data<0) zero_height = 0;
-        else if (min_data>0) zero_height = this.props.height;
-        else zero_height = this.props.height*(max_data/(max_data-min_data));
-
-        // helper function to compute the height since it is used in the
-        // 'height' attribute as well as the 'y' attribute
-        var h = function(d,i){
-            return Math.max(Math.abs(d)/y_range*this.props.height,1)
-        }.bind(this);
+        var y_scale = d3.scaleLinear()
+            .domain([min_data, max_data])
+            .range([0,this.props.height]);
+        var zero_height = this.props.height-y_scale(0);
+        var h = function (d) {return Math.abs(y_scale(d)-y_scale(0)) || 1;};
+        var y = function (d) {return d<0 ? zero_height : zero_height - h(d);};
 
         d3.select('#'+this.props.graph_id)
             .selectAll('rect')
             .data(this.props.data)
             .enter()
             .append('rect')
-            .attr('x', function(d,i){
-                return i*(width+1)
-            })
-            .attr('y', function(d,i){
-                return d<0 ? zero_height : zero_height - h(d,i);
-            })
-            .attr('width',width)
+            .attr('x', function(d,i){return i*(w+1)})
+            .attr('y',y)
+            .attr('width', w)
             .attr('height', h)
             .attr('fill', function(d,i){
                 if (d<0) return 'rgb(204,0,0)'
@@ -54,7 +44,7 @@ var TimeSeries = React.createClass({
 ReactDOM.render(
     <div>
         <h1>GlassTaxes</h1>
-        <TimeSeries height={300} width={2 00} data={test_data} graph_id={'total_budget_spend'}/>
+        <TimeSeries height={300} width={200} data={test_data} graph_id={'total_budget_spend'}/>
     </div>,
     document.getElementById('content')
 )
